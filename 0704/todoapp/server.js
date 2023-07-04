0b952599a493648f2d4e38b1373aef271f780d58
chaeyoung <chaemaa00@gmail.com>
2023-07-04 15:29:33 +0900

    해당 서버에 띄울 파일 연결(요청)하기

--- 0704/todoapp/server.js
+++ 0704/todoapp/server.js
@@ -4,19 +4,10 @@ app = express();
 app.listen(8080, function () {
   console.log("Listening on 8080");
 });
-//요청 처리하는 GET
-//GET요청은 url에 입력하는 것이다.
 
-//누군가가 /intro으로 url을 작성해 방문하면 intro관련된 안내문을 띄워주자
+//.sendFile("보낼 파일 경로")
+// 서버에 파일 요청하기
 
-// app.get("경로", function (요청, 응답) {
-//     응답.send("반갑습니다");
-// });
-
-app.get("/intro", function (req, res) {
-  res.send("Hello");
-});
-
-app.get("/list", function (req, res) {
-  res.send("TO DO LIST");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
