fccb4f4f13720cce82e0f1cbeb85ac8d397ec022
chaeyoung <chaemaa00@gmail.com>
2023-07-04 15:04:05 +0900

    성공적으로 지정된 서버에 들어오면 메세지 띄우기

--- 0704/todoapp/server.js
+++ 0704/todoapp/server.js
@@ -4,4 +4,7 @@ app = express();
 app.listen(8080, function () {
   //listen(파라미터1, 파라미터2) => listen(서버 띄울 포트번호, 띄운 후 실행할 코드)
   // 8080 port에 서버를 띄워주세요!
+  console.log("Listening on 8080");
+  //8080 port에 무사히 들어오면 "Listening on 8080"메세지를 띄워주세요.
 });
+// 서버 접속하는 방법은 주소창에 localhost:8080입력하기
