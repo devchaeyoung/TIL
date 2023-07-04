2348106066dbc3ef82b653a4d140ec5bf180eb47
chaeyoung <chaemaa00@gmail.com>
2023-07-04 14:40:07 +0900

    서버띄울 포트번호 지정해주기

--- 0704/todoapp/server.js
+++ 0704/todoapp/server.js
@@ -1,4 +1,7 @@
 const express = require("express");
 app = express();
 
-app.listen();
+app.listen(8080, function () {
+  //listen(파라미터1, 파라미터2) => listen(서버 띄울 포트번호, 띄운 후 실행할 코드)
+  // 8080 port에 서버를 띄워주세요!
+});
