18411a3695ba05f55ddb2d5932de1d414afd4822
chaeyoung <chaemaa00@gmail.com>
2023-07-04 17:22:53 +0900

    생활코딩 node.js강의로 공부시작

--- 0704/web/main.js
+++ 0704/web/main.js
@@ -1,40 +1,19 @@
 var http = require("http");
 var fs = require("fs");
-const url = require("url");
+var url = require("url");
 
-var app = http.createServer(function (request, response) {
-  const _url = request.url;
-  var queryData = url.parse(request.url, true).query;
-  console.log(queryData);
-
+var app = http.createServer(function (req, res) {
+  var _url = req.url;
+  var queryData = url.parse(_url, true).query;
+  console.log(queryData.id);
   if (_url == "/") {
-    url = "/index.html";
+    _url = "/index.html";
   }
-  if (_url == "/favicon.ico") {
-    return response.writeHead(404);
+  if (_url == "favicon.ico") {
+    return res.writeHead(404);
   }
-  response.writeHead(200);
-
-  const template = `<!doctype html>
-  <html>
-  <head>
-    <title>WEB1 - ${template}</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="/?id=index">WEB</a></h1>
-    <ol>
-      <li><a href="/?id=HTML">HTML</a></li>
-      <li><a href="/?id=CSS">CSS</a></li>
-      <li><a href="/?id=JavaScript">JavaScript</a></li>
-    </ol>
-    <h2>${template}</h2>
-    <p>The World Wide Web (abbreviated WWW or the Web) is an information space where documents and other web resources are identified by Uniform Resource Locators (URLs), interlinked by hypertext links, and can be accessed via the Internet.[1] English scientist Tim Berners-Lee invented the World Wide Web in 1989. He wrote the first web browser computer program in 1990 while employed at CERN in Switzerland.[2][3] The Web browser was released outside of CERN in 1991, first to other research institutions starting in January 1991 and to the general public on the Internet in August 1991.
-    </p>
-  </body>
-  </html>
-  `;
-
-  response.end(fs.readFileSync(__dirname + _url));
+  res.writeHead(200);
+  res.end(fs.readFileSync(__dirname + _url));
 });
-app.listen(3001);
+
+app.listen(3000);
